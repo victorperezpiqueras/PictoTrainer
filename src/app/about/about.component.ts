9e2742eb1f0fb6c99399d85547c88d67a27e3b5c
joseramonmartinez <josera.martinez@hotmail.com>
2020-02-23 14:18:17 +0100

    boton sonido

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,28 +10,17 @@ import { environment } from '@env/environment';
 })
 export class AboutComponent implements OnInit {
   version: string | null = environment.version;
-  barColor: string;
+  checked = false;
+  isOn = false;
   constructor() {}
 
-  ngOnInit() {
-    this.barColor = localStorage.getItem('bar-color');
-    if (!this.barColor || this.barColor != undefined || this.barColor != null) this.barColor = '#FFEA82';
-    //var ProgressBar = require('progressbar.js')
-    var line = new ProgressBar.Line('#container');
-    var bar = new ProgressBar.Line('#container', {
-      strokeWidth: 30,
-      duration: 10000,
-      color: this.barColor,
-      trailColor: '#eee',
-      trailWidth: 30,
-      svgStyle: { width: '100%', height: '100%' },
-      from: { color: this.barColor },
-      to: { color: '#ED6A5A' },
-      step: (state: any, bar: any) => {
-        bar.path.setAttribute('stroke', state.color);
-      }
-    });
+  ngOnInit() {}
 
-    bar.animate(1.0);
+  state1() {
+    if (!this.isOn) {
+      this.isOn = true;
+    } else {
+      this.isOn = false;
+    }
   }
 }
